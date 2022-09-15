4e3e06ae7c5703d89a3ee2e8886596a5964e1627
0xsnowman <eth.snowman@gmail.com>
2022-09-15 20:07:13 +0800

    fix-atom-process-attr

--- src/ui/atoms/process.tsx
+++ src/ui/atoms/process.tsx
@@ -2,15 +2,21 @@ import React from "react";
 
 interface IProcess {
   value: number;
+  height?: number;
+  borderRadius?: number;
 }
 
-const Process: React.FC<IProcess> = ({ value }) => {
+const Process: React.FC<IProcess> = ({ value, height = 2, borderRadius = 0 }) => {
   return (
-    <div className="atom-process">
+    <div className="atom-process" style={{
+      borderRadius: borderRadius
+    }}>
       <div
         className="atom-process__fill"
         style={{
-          width: `${value * 100}%`
+          width: `${value * 100}%`,
+          height: height,
+          borderRadius: borderRadius
         }}
       ></div>
     </div>
