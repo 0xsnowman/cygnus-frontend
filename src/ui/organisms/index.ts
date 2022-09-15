8c436eff65380f9aaaf2db5845373e0fc48c22b9
0xsnowman <eth.snowman@gmail.com>
2022-09-16 01:52:16 +0800

    fix-molecule-organism-index-files-fix

--- src/ui/organisms/index.ts
+++ src/ui/organisms/index.ts
@@ -1,10 +1,11 @@
 import Header from './header';
 import Footer from './footer';
 import DepositTab from './depositTab';
+import Switch from './switch';
 import LendingPoolCard from './lendingPoolCard';
 import LendingPoolTab from './lendingPoolTab';
 import LendingPoolView from './lendingPoolView';
 
 export {
-    Header, Footer, DepositTab, LendingPoolCard, LendingPoolTab, LendingPoolView, 
+    Header, Footer, DepositTab, LendingPoolCard, LendingPoolTab, LendingPoolView, Switch
 }
