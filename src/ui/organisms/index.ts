2a40bc1e829feedde7b322e53d6383c531419b88
0xsnowman <eth.snowman@gmail.com>
2022-09-15 23:38:58 +0800

    fix-organism-index-change-deposittab

--- src/ui/organisms/index.ts
+++ src/ui/organisms/index.ts
@@ -1,9 +1,10 @@
 import Header from './header';
 import Footer from './footer';
+import DepositTab from './depositTab';
 import LendingPoolCard from './lendingPoolCard';
 import LendingPoolTab from './lendingPoolTab';
 import LendingPoolView from './lendingPoolView';
 
 export {
-    Header, Footer, LendingPoolCard, LendingPoolTab, LendingPoolView
+    Header, Footer, DepositTab, LendingPoolCard, LendingPoolTab, LendingPoolView, 
 }
