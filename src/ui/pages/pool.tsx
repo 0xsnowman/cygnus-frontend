3c514b1c83c717c4f79558bf75a7d84e60b83105
0xsnowman <eth.snowman@gmail.com>
2022-09-15 20:08:21 +0800

    fix-page-pool-header-add

--- src/ui/pages/pool.tsx
+++ src/ui/pages/pool.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { Box, Container, Flex, Icon, Text } from "../atoms";
+import { Box, Container, Flex, Icon, Text, Process } from "../atoms";
+import { Button } from "../molecules";
 
 import coinPng from "../../assets/icons/coin.png";
 import logoPng from "../../assets/icons/logo.png";
@@ -13,68 +14,38 @@ const Pool = () => {
     <Page>
       <Container>
         <Box className="page-pool">
-          <Flex flexDirection="column" gap={15} alignItems="center">
-            <Box>
-              <Text fontSize="MEDIUM">Spirit Boosted</Text>
-            </Box>
-            <Box>
-              <Text>ETH - AVAX</Text>
-            </Box>
-            <Flex gap={10} alignItems="center">
-              <Text color="#BBB">Deposited</Text>
-              <Icon src={logoPng} size="LINK" />
-              <Text color="#BBB">LP</Text>
-            </Flex>
-            <Text>$555,954</Text>
-            <Box position="relative" width="56%">
-              <Box position="absolute" width="99.4%" top={25}>
-                <Flex justifyContent="space-between" gap={120}>
-                  <Box
-                    borderTop="1px dotted white"
-                    borderLeft="1px dotted white"
-                    width="40%"
-                    height="20px"
-                  />
-                  <Box
-                    borderTop="1px dotted white"
-                    borderRight="1px dotted white"
-                    width="40%"
-                    height="20px"
-                  />
+          <Flex flexDirection="column" gap={20}>
+            <Flex alignItems="center" justifyContent="space-around" gap={20}>
+              <Flex flexDirection="column" gap={20}>
+                <Flex gap={10}>
+                  <Button backgroundColor="#073549">COVERED CALL</Button>
+                  <Button backgroundColor="#073549">V2</Button>
                 </Flex>
-              </Box>
-              <TokenPair token1={coinPng} token2={coinPng} />
-            </Box>
-            <Flex flexDirection="column" gap={20} paddingVertical={30} width="60%">
-              <Flex justifyContent="space-between">
-                <Text>$306k</Text>
-                <Text color="grey">Total Supply</Text>
-                <Text>$800k</Text>
+                <Text fontSize="EXTRA_LARGE">ETH-AVAX-C</Text>
+                <Flex justifyContent="space-between">
+                  <Text color="#999">
+                    Current Vault Deposits
+                  </Text>
+                  <Text>
+                    13.07K AVAX
+                  </Text>
+                </Flex>
+                <Process value={0.4} height={15} borderRadius={10} />
+                <Flex justifyContent="space-between">
+                  <Text color="#999">
+                    Max Vault Capacity
+                  </Text>
+                  <Text>
+                    200K AVAX
+                  </Text>
+                </Flex>
               </Flex>
-              <Flex justifyContent="space-between">
-                <Text>$306k</Text>
-                <Text color="grey">Total Borrowed</Text>
-                <Text>$800k</Text>
-              </Flex>
-              <Flex justifyContent="space-between">
-                <Text>$306k</Text>
-                <Text color="grey">Total Lend</Text>
-                <Text>$800k</Text>
-              </Flex>
-              <Flex justifyContent="space-between">
-                <Text>$306k</Text>
-                <Text color="grey">Total APR</Text>
-                <Text>$800k</Text>
-              </Flex>
-              <Flex justifyContent="space-between">
-                <Text>$306k</Text>
-                <Text color="grey">Borrowed APR</Text>
-                <Text>$800k</Text>
+              <Flex alignItems="center">
+                <Icon src={logoPng} size="EXTRA_LARGE" />                
               </Flex>
             </Flex>
-          </Flex>
-          <Flex flexDirection="column">
-            <LendingPoolView />
+            <Flex>
+            </Flex>
           </Flex>
         </Box>
       </Container>
