74d90160d1cbf0567416621e40b2f90cf078f178
0xsnowman <eth.snowman@gmail.com>
2022-09-15 23:39:28 +0800

    fix-page-pool-change

--- src/ui/pages/pool.tsx
+++ src/ui/pages/pool.tsx
@@ -3,11 +3,9 @@ import React from "react";
 import { Box, Container, Flex, Icon, Text, Process } from "../atoms";
 import { Button } from "../molecules";
 
-import coinPng from "../../assets/icons/coin.png";
 import logoPng from "../../assets/icons/logo.png";
-import TokenPair from "../molecules/tokenPair";
 import { Page } from "../molecules";
-import { LendingPoolView } from "../organisms";
+import { DepositTab } from "../organisms";
 
 const Pool = () => {
   return (
@@ -16,35 +14,44 @@ const Pool = () => {
         <Box className="page-pool">
           <Flex flexDirection="column" gap={20}>
             <Flex alignItems="center" justifyContent="space-around" gap={20}>
-              <Flex flexDirection="column" gap={20}>
+              <Flex flexDirection="column" flex={1} gap={20}>
                 <Flex gap={10}>
                   <Button backgroundColor="#073549">COVERED CALL</Button>
                   <Button backgroundColor="#073549">V2</Button>
                 </Flex>
                 <Text fontSize="EXTRA_LARGE">ETH-AVAX-C</Text>
                 <Flex justifyContent="space-between">
-                  <Text color="#999">
-                    Current Vault Deposits
-                  </Text>
-                  <Text>
-                    13.07K AVAX
-                  </Text>
+                  <Text color="#999">Current Vault Deposits</Text>
+                  <Text>13.07K AVAX</Text>
                 </Flex>
                 <Process value={0.4} height={15} borderRadius={10} />
                 <Flex justifyContent="space-between">
-                  <Text color="#999">
-                    Max Vault Capacity
-                  </Text>
-                  <Text>
-                    200K AVAX
-                  </Text>
+                  <Text color="#999">Max Vault Capacity</Text>
+                  <Text>200K AVAX</Text>
                 </Flex>
               </Flex>
-              <Flex alignItems="center">
-                <Icon src={logoPng} size="EXTRA_LARGE" />                
+              <Flex alignItems="center" justifyContent="center" flex={1}>
+                <Icon src={logoPng} size="EXTRA_LARGE" />
               </Flex>
             </Flex>
-            <Flex>
+            <Flex justifyContent="space-around">
+              <Flex flexDirection="column" flex={1} gap={20}>
+                <Text>VAULT STRATEGY</Text>
+                <Text color="#999">
+                  The vault earns yield on its sAVAX deposits by running a
+                  weekly automated sAVAX covered call strategy where it stakes
+                  its sAVAX deposits in Benqi and then uses its sAVAX to
+                  collateralize weekly out-of-money sAVAX call options. The
+                  yield earned from both the covered call strategy and the sAVAX
+                  staking rewards are reinvested weekly, effectively compounding
+                  the yields for depositors over time.
+                </Text>
+              </Flex>
+              <Flex flex={1} justifyContent="center" alignItems="center">
+                <Box padding={30}>
+                  <DepositTab />
+                </Box>
+              </Flex>
             </Flex>
           </Flex>
         </Box>
